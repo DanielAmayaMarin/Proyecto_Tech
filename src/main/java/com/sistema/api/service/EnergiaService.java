e1b7b3d7231bd121a007c55b2ab5d734f90c1802
danielamayam <105296748+danielamayam@users.noreply.github.com>
2024-10-27 10:58:36 -0500

    Se implementa la lectura de procedimiento almacenados

--- src/main/java/com/sistema/api/service/EnergiaService.java
+++ src/main/java/com/sistema/api/service/EnergiaService.java
@@ -4,6 +4,8 @@ import com.sistema.api.model.Energia;
 import com.sistema.api.repository.EnergiaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -11,22 +13,31 @@ public class EnergiaService {
     @Autowired
     private EnergiaRepository energiaRepository;
 
+    public EnergiaService(EnergiaRepository energiaRepository) {
+        this.energiaRepository = energiaRepository;
+    }
+
+    @Transactional
     public List<Object[]> getProduccionTotalPorTipoYAnio(String anio) {
         return energiaRepository.getProduccionTotalPorTipoYAnio(anio);
     }
 
+    @Transactional
     public List<Object[]> getTop10PaisesProduccionEolica(String anio) {
         return energiaRepository.getTop10PaisesProduccionEolica(anio);
     }
 
+    @Transactional
     public List<Float> getPorcentajeEnergiaRenovablePorRegion(String anio) {
         return energiaRepository.getPorcentajeEnergiaRenovablePorRegion(anio);
     }
 
+    @Transactional
     public List<Object[]> getTendenciaEnergíaSolar() {
-        return energiaRepository.getTendenciaEnergíaSolar();
+        return energiaRepository.getTendenciaEnergiaSolar();
     }
 
+    @Transactional
     public Energia guardarEnergia(Energia energia) {
         return energiaRepository.save(energia);
     }
